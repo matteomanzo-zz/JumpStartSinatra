1147f9b7e916e993a4334e6e217c028701457442
matteomanzo <imatteo.manzo@gmail.com>
2015-02-09 09:09:22 +0000

    login, logout

--- songs_by_sinatra/main.rb
+++ songs_by_sinatra/main.rb
@@ -1,18 +1,52 @@
 require 'sass'
 require 'sinatra'
+require 'sinatra/reloader' if development?
 require 'slim'
 require './song'
 
 set :views, Proc.new { File.join(root, "/views") }
 
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+  set :session_secret, 'session persists'
+
 get '/styles.css' do
   scss :styles
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params][:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/songs')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/login')
+end
+
+get '/set/:name' do
+  session[:name] = params[:name]
+end
+
+get '/get/hello' do
+  "hello #{session[:name]}"
 end
 
 get '/' do
   slim :home
 end
-
 
 get '/about' do
   @title = "All About This Website"
@@ -21,42 +55,6 @@ end
 
 get '/contact' do
   slim :contact
-end
-
-get '/songs' do
-  @songs = Song.all
-  slim :songs
-end
-
-get '/songs/new' do
-  @song = Song.new
-  slim :new_song
-end
-
-get '/songs/:id' do
-  @song = Song.get(params[:id])
-  slim :show_song
-end
-
-post '/songs' do
-  song = Song.create(params[:song])
-  redirect to("/songs/#{song.id}")
-end
-
-get '/songs/:id/edit' do
-  @song = Song.get(params[:id])
-  slim :edit_song
-end
-
-put '/songs/:id' do
-  song = Song.get(params[:id])
-  song.update(params[:song])
-  redirect to("songs/#{song.id}")
-end
-
-delete '/songs/:id' do
-  Song.get(params[:id]).destroy
-  redirect to('/songs')
 end
 
 not_found do
--- songs_by_sinatra/song.rb
+++ songs_by_sinatra/song.rb
@@ -1,7 +1,9 @@
 require 'dm-core'
 require 'dm-migrations'
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
 
 class Song
  include DataMapper::Resource
@@ -14,7 +16,44 @@ class Song
  def released_on=date
   super Date.strptime(date, '%m/%d/%Y')
  end
- 
+
 end
 
 DataMapper.finalize
+
+get '/songs' do
+  @songs = Song.all
+  slim :songs
+end
+
+get '/songs/new' do
+  @song = Song.new
+  slim :new_song
+end
+
+get '/songs/:id' do
+  @song = Song.get(params[:id])
+  slim :show_song
+end
+
+post '/songs' do
+  song = Song.create(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+get '/songs/:id/edit' do
+  @song = Song.get(params[:id])
+  slim :edit_song
+end
+
+put '/songs/:id' do
+  song = Song.get(params[:id])
+  song.update(params[:song])
+  redirect to("songs/#{song.id}")
+end
+
+delete '/songs/:id' do
+  halt(401, 'Not Authorized') unless session[:admin]
+  Song.get(params[:id]).destroy
+  redirect to('/songs')
+end
